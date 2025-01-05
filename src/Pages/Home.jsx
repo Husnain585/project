72834070194d03d39b31419d0f2eea5fb38f846d
husnain585 <husnain7t9@gmail.com>
2025-01-05 23:56:56 +0500

    Changing

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import SummarySales from "../Features/MainComponent/SummarySales";
 import Banner from "../Features/MainComponent/Banner";
 import WhyTrustUs from "../Features/MainComponent/whyTrustUs";
 import SaleSection from "../Features/MainComponent/SalesSection";
 import CounterUp from "../Features/MainComponent/counterFunction";
 import Testimonials from "../Features/MainComponent/Testimonials";
+import "../App.css"
 
 const Home = ({ children }) => {
   return (
-    <div className="w-full min-h-screen flex flex-col">
+    <div className="w-full min-h-screen flex flex-col appear">
       <div className="flex-1">
         {children}
         <Banner />
-        <WhyTrustUs/>
+        <WhyTrustUs />
         <Testimonials />
         <SaleSection />
         <CounterUp />
@@ -22,3 +22,10 @@ const Home = ({ children }) => {
 };
 
 export default Home;
+
+
+
+/* Optional Intersection Observer for better support */
+/* .appear.observed {
+    animation: appear 1s ease-out both;
+} */
