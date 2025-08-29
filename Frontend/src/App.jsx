8ef3e74e6f8fb51f4dcbae968ea619695482c283
husnain585 <husnain7t9@gmail.com>
2025-08-29 05:27:59 -0700

    Front vite/react config successfully

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+function App() {
+
+  return (
+    <>
+      <div className="bg-red-100">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
